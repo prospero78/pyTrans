97b21d24ede5ba6a2937d89d285dbeb38c6aca2e
prospero78su <prospero.78.su@gmail.com>
2015-12-15 21:18:14 +0200

    107 Тотальная пересадка сервера с голого Tcp на Http

--- Server/pakTransServ/pakControl/pakServerThread/modServerThread.py
+++ Server/pakTransServ/pakControl/pakServerThread/modServerThread.py
@@ -1,37 +1,22 @@
 # -*- coding: utf-8 -*-
 '''
-В этом потоке сервер ожидает подключений от киентов, после чего
-перебрасывает их на свои потоки.
+В этом потоке сервер создаёт один поток. Он принимает запрос на 
+стандартный порт сервера HTTP .
+После чего работает с дескриптором клиента.
 '''
 
 from PySide import QtCore
-from .pakTcpServer.modTcpServer import clsTcpServer
+from .pakHttpRequest.modHttpRequest import clsHttpRequest
+from http.server import HTTPServer as clsHttpServer
 
 class clsServerThread(QtCore.QThread):
     def __init__(self, root):
         print('      clsServerThread.__init__()')
         self.__root = root
-        self.__TcpServer = clsTcpServer(root)
         self.running = False
         super(clsServerThread, self).__init__()
         
     def run(self):
-        print('      clsServerThread.run()')
-        self.__TcpServer.adress = 'localhost'
-        self.sleep(1)
-        self.__TcpServer.port = int(self.__root.Gui.winMain.entPort.text())
-        print('      run 1()')    
-        self.sleep(1)
-        self.__TcpServer.run()
-        print('      run 2()')    
-        self.sleep(1)
-        while self.running:
-            if self.__TcpServer.listening:
-                print('      clsServerThread listening')   
-            else:
-                print('      clsServerThread not listening')  
-            self.sleep(3)
-        else:
-            print('      clsServerThread stopped')
-            self.__TcpServer.stop()
-            #self.__TcpServer.stop()
+        print('      clsServerThread.serve_forever()')
+        self.__srv = clsHttpServer(('', 80),  clsHttpRequest)
+        self.__srv.serve_forever()
